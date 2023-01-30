0e9da2938464f4b0eae6e82bbe8df6491069a254
iammilan07 <98943689+iammilan07@users.noreply.github.com>
2023-01-30 14:20:06 +0545

    szc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
+import Login from "./screens/Login";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
