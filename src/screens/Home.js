2cb107c648bffaf2407d6c8f3b1915ef78ac0c6a
iammilan07 <98943689+iammilan07@users.noreply.github.com>
2023-01-30 13:00:10 +0545

    aip

---
+++ src/screens/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <Nav />
+        <div>Body</div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
